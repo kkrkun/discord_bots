62b3df8284faae845dcefe60ec1d40fddb614299
kkrkun <110749983+kkrkun@users.noreply.github.com>
2024-10-29 13:02:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
-const http = require('http');
-http.createServer(function(request, response)
-{
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Bot is online!');
-}).listen(3000);
+const { Client, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js')
 
 console.log("Botを起動しました。");
 
