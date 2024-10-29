01d0da2f5505bbcc7aad9cb02203e5a697237178
kkrkun <110749983+kkrkun@users.noreply.github.com>
2024-10-29 23:39:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const http = require('http');
+http.createServer(function(request, response)
+{
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Bot is online!');
+}).listen(8000);
+
 console.log("Botを起動しました。");
 require('./jrpg.js');
 require('./aooni.js');
